997237c35468159d18d07965085be7e5da2334b4
Mikhail Golubev <mvgolubev@gmail.com>
2024-08-20 15:39:45 +0300

    Fix send_text function

--- send_msg_from_bot.py
+++ send_msg_from_bot.py
@@ -4,17 +4,23 @@ from bot_instance import bot
 
 async def send_text(ids: list[int]) -> None:
     text = (
-        "Привет!\nБот недавно помог вам найти ИИН. "
+        "Привет!\n🤖 Бот недавно помог вам найти ИИН. "
         "Теперь вы можете помочь боту своим "
-        "<a href='https://pay.cloudtips.ru/p/9d2b07f7'>пожертвованием</a>"
+        "<a href='https://pay.cloudtips.ru/p/9d2b07f7'>пожертвованием</a>\n"
+        "🥹 Спасибо за поддержку!"
     )
     for tg_id in ids:
-        await bot.send_animation(
-            chat_id=tg_id,
-            animation="CgACAgIAAxkBAAIfmGbBMa7x0K4rG-GIu66Vvh04m7nZAAIaUgACFQgRSt0QJMcbESvRNQQ",
-            caption=text,
-        )
-        await asyncio.sleep(5)
+        print(tg_id)
+        try:
+            async with bot:
+                await bot.send_message(
+                    chat_id=tg_id,
+                    text=text,
+                    disable_web_page_preview=True,
+                )
+        except Exception as err:
+            print(err)
+        await asyncio.sleep(2)
 
 
 if __name__ == "__main__":
