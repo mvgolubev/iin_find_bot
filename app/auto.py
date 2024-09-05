c657c1bf1693daf4fdda3f74680c775a1ba89639
Mikhail Golubev <mvgolubev@gmail.com>
2024-09-05 12:20:38 +0300

    Fix try-except block

--- app/auto.py
+++ app/auto.py
@@ -30,20 +30,20 @@ async def search(repeat_minutes: int) -> None:
                     rowid=log_row_num, cache_used=0, iins_found=iins_found
                 )
                 if iins_found:
-                    await bot.send_sticker(
-                        chat_id=task["tg_id"],
-                        sticker="CAACAgIAAxkBAAIdWWa1K79rFVg3jNUczO4Cdg0Ug9LkAAJHAgACRxVoCeT3B7UIQsMeNQQ",
-                    )
-                    text = "🎉 <b>Авто-поиск нашёл ИИН:</b>\n\n"
-                    for iin in iins_found:
-                        text += f"<b>ИИН:</b> <code>{iin['iin']}</code>\n"
-                        full_name = utils.get_full_name(iin)
-                        text += f"<b>ФИО:</b> {full_name}\n\n"
-                    text += (
-                        "<i>(ткните в значение ИИН, чтобы скопировать его в буфер)</i>\n\n"
-                        'Сказать спасибо можно по кнопке <b>"Donate"</b>'
-                    )
                     try:
+                        await bot.send_sticker(
+                            chat_id=task["tg_id"],
+                            sticker="CAACAgIAAxkBAAIdWWa1K79rFVg3jNUczO4Cdg0Ug9LkAAJHAgACRxVoCeT3B7UIQsMeNQQ",
+                        )
+                        text = "🎉 <b>Авто-поиск нашёл ИИН:</b>\n\n"
+                        for iin in iins_found:
+                            text += f"<b>ИИН:</b> <code>{iin['iin']}</code>\n"
+                            full_name = utils.get_full_name(iin)
+                            text += f"<b>ФИО:</b> {full_name}\n\n"
+                        text += (
+                            "<i>(ткните в значение ИИН, чтобы скопировать его в буфер)</i>\n\n"
+                            'Сказать спасибо можно по кнопке <b>"Donate"</b>'
+                        )
                         await bot.send_message(
                             chat_id=task["tg_id"], text=text, reply_markup=kb.info
                         )
